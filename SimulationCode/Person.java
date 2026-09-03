5856ed5fa279ccb7ec6e8f718da89a752e737aca
Shoumik Ghosh <pantumdelux@gmail.com>
2026-09-03 14:12:09 +1000

    Adding movement variables for Person

--- SimulationCode/Person.java
+++ SimulationCode/Person.java
@@ -2,15 +2,47 @@ public class Person {
     private String name;
     private int age;
 
+    private float x;
+    private float y;
+    private float speedX;
+    private float speedY;
+
     public Person(String name, int age){
         this.name = name;
         this.age = age;
+
+        x = 100;
+        y = 100;
+        speedX = 1;
+        speedY = 1;
     }
+
     public String getName() {
         return name;
     }
+
     public int getAge(){
         return age;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;   
+    }    
+
+    public void move(){
+        x += speedX;
+        y += speedY;
+
+        if(x < 0 || x > 500){
+            speedX *= -1;
+        }
+        if(y < 0 || y > 500){
+            speedY *= -1;
+        }
     }
 
     public String getStatus() {
