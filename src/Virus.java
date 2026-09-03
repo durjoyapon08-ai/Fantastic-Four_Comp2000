6c36537936d6c1de111f5f9cfd83067dbd74b9d4
apnacollege-demo <durjoy.apon08@gmail.com>
2026-09-04 09:09:45 +1000

    Add Virus class

---
+++ src/Virus.java
@@ -0,0 +1,27 @@
+public class Virus {
+
+    private String virusName;
+    private int spreadPercentage;
+
+    public Virus(String virusName, int spreadPercentage) {
+        this.virusName = virusName;
+        this.spreadPercentage = spreadPercentage;
+    }
+
+    public String getVirusName() {
+        return virusName;
+    }
+
+    public int getSpreadPercentage() {
+        return spreadPercentage;
+    }
+
+    public void setSpreadPercentage(int spreadPercentage) {
+        this.spreadPercentage = spreadPercentage;
+    }
+
+    @Override
+    public String toString() {
+        return virusName + " - " + spreadPercentage + "%";
+    }
+}
