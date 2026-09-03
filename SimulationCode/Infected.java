092bb83cc78efd0fe6bde3487c0b4f65b92b1ad5
Shoumik Ghosh <pantumdelux@gmail.com>
2026-09-03 14:08:07 +1000

    Adding InfectionDuration

--- SimulationCode/Infected.java
+++ SimulationCode/Infected.java
@@ -1,26 +1,37 @@
 public class Infected extends Person{
     private Virus virus;
+    private int infectionDuration;
 
     public Infected(String name, int age, Virus virus){
         super(name, age);
         this.virus = virus;
+        this.infectionDuration = 0;
     }
+    
     public Virus getVirus(){
         return virus;
     }
 
-    public Person checkCondition(){
-        int percentage = virus.getSpreadPercentage();
-
-        if(percentage <= 20){
-            return new Recovered(getName(), getAge(), virus);
-        }
-        else if (percentage > 70) {
-            return new Dead(getName(), getAge(), virus);
-        } else {
-            return this;
-        }
+    public void increaseInfectionDuration(){
+        infectionDuration++;
     }
+
+    public int getInfectionDuration(){
+        return infectionDuration;
+    }
+
+    // public Person checkCondition(){
+    //     int percentage = virus.getSpreadPercentage();
+
+    //     if(percentage <= 20){
+    //         return new Recovered(getName(), getAge(), virus);
+    //     }
+    //     else if (percentage > 70) {
+    //         return new Dead(getName(), getAge(), virus);
+    //     } else {
+    //         return this;
+    //     }
+    // }
 
     @Override
     public String getStatus() {
