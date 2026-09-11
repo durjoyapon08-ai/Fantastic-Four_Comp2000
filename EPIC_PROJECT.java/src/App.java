3d16135dfd972544dcc3ae31aee32d3dddc117e9
Durjoy Paul Apon <durjoy.apon08@gmail.com>
2026-09-11 16:14:22 +1000

    Remove unrelated EPIC project

--- EPIC_PROJECT.java/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-
-        System.out.println("The city of Gotham");
-        
-        
-        PanelDemo.main(args);
-    }
-}
